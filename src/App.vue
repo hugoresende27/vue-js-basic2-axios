e318c3cb24845f4c676dd8d40edb0ffc629aac5d
hugo.resende <hugo.resende@devoteam.com>
2025-03-23 10:22:30 +0000

    first

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,104 @@
 <template>
-<!--  <HelloWorld msg="Hugo Boss"/>-->
   <div>
-    <h1 v-html="this.question">
-    </h1>
-    <input
-        type="radio"
-        name="options"
-        value="True">
-    <label>True</label><br>
-    <input
-        type="radio"
-        name="options"
-        value="False">
-    <label>False</label><br>
-    <button class="send" type="button">Send</button>
+
+    <ScoreBoard :winCount="this.winCount" :lossCount="this.lossCount"/>
+
+    <template v-if="this.question">
+
+      <h1 v-html="this.question">
+      </h1>
+
+      <template v-for="(answer, index) in this.answers" :key="index">
+        <input
+            :disabled="this.answerSubmitted"
+            type="radio"
+            name="options"
+            :id="'option-' + index"
+            :value=answer
+            v-model="chosenAnswer">
+
+        <label :for="'option-' + index" v-html="answer"></label><br />
+      </template>
+
+      <button @click="this.submitAnswer"  class="send" type="button">Send</button>
+
+      <section class="result" v-if="this.answerSubmitted">
+        <template v-if="this.chosenAnswer === this.correctAnswer">
+          <h4 v-html="'&#9989; Congratulations, the answer' + this.correctAnswer + ' is correct.'"></h4>
+        </template>
+        <template v-else>
+          <h4 v-html="'&#10060;  I`m sorry, you picked the wrong answer. The correct is ' + this.correctAnswer +'.'"></h4>
+        </template>
+        <button @click="this.getNewQuestion()" class="send" type="button">Next question</button>
+      </section>
+
+    </template>
+
 
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
 
+import ScoreBoard from "@/components/ScoreBoard.vue";
 export default {
   name: 'App',
   components: {
-    // HelloWorld
+    ScoreBoard
   },
   data() {
     return {
      question : undefined,
       incorrectAnswers : [],
-      correctAnswer : undefined
+      correctAnswer : undefined,
+      chosenAnswer: undefined,
+      answerSubmitted: false,
+      winCount: 0,
+      lossCount: 0
     }
   },
   computed: {
     answers() {
       let answers = [...this.incorrectAnswers];
       // let answers = JSON.parse( JSON.stringify(this.incorrectAnswers));
-      answers.push(this.correctAnswer);
+      // answers.push(this.correctAnswer);
+      answers.splice(Math.round(Math.random() * answers.length),0,this.correctAnswer);
       return answers;
+    },
+  },
+  methods: {
+    submitAnswer() {
+      if (!this.chosenAnswer) {
+        alert('Pick one of the options')
+      } else {
+        this.answerSubmitted = true;
+        if(this.chosenAnswer === this.correctAnswer) {
+          console.log('Win')
+          this.winCount ++;
+        } else {
+          console.log('Loose')
+          this.lossCount ++;
+        }
+      }
+    },
+    getNewQuestion() {
+      this.answerSubmitted = false;
+      this.chosenAnswer = undefined;
+      this.question = undefined;
+
+      let apiUrl = 'https://opentdb.com/api.php?amount=1&category=18';
+      this.axios.
+      get(apiUrl).
+      then((response) => {
+        this.question = response.data.results[0].question;
+        this.correctAnswer = response.data.results[0].correct_answer;
+        this.incorrectAnswers = response.data.results[0].incorrect_answers;
+        console.log(this.correctAnswer)
+      })
     }
   },
   created() {
-    let apiUrl = 'https://opentdb.com/api.php?amount=1&category=18';
-    this.axios.
-    get(apiUrl).
-    then((response) => {
-      this.question = response.data.results[0].question;
-      this.correctAnswer = response.data.results[0].correct_answer;
-      this.incorrectAnswers = response.data.results[0].incorrect_answers;
-      console.log(response.data.results[0])
-      // let res = response.data.results;
-    })
+    this.getNewQuestion()
   }
 }
 
@@ -94,11 +141,11 @@ export default {
     border-bottom: 1px solid black;
     padding: 24px;
     font-size: 18px;
-
     span {
-      padding: 8px;
+      padding: 10px;
       font-weight: bold;
       border: 1px solid black;
+      margin: 5px;
     }
   }
 }
---
+++ src/components/ScoreBoard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <section class="score">
+      Player <span>{{this.winCount}}</span> x
+      <span>{{this.lossCount}}</span> Computer
+    </section>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['winCount', 'lossCount']
+  }
+</script>
+
+<style lang="scss">
+
+</style>
